29730bdec1cd19dbc96ae05d798ae754cd8519ee
Kanchimalai <kanchimalai.m2023ai-ds@sece.ac.in>
2025-12-17 10:11:06 +0530

    third commit

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,9 +1,9 @@
+import { io } from "socket.io-client";
 
-import { io } from 'socket.io-client';
-
-
-const SOCKET_SERVER_URL = "https://chat-backend-api1.onrender.com"; 
+const SOCKET_SERVER_URL = "https://chat-backend-api1.onrender.com";
 
 export const socket = io(SOCKET_SERVER_URL, {
-  
+  withCredentials: true,
+  transports: ["websocket", "polling"],
+  autoConnect: true,
 });
